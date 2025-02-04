cca804218f6edfe4c03f8035279583fcf59432aa
Ricardo Uehlein <ricardo.uehlein@wexinc.com>
2025-02-04 14:00:10 -0300

    Add GeneralMediaPOO project and implement Student class
    Updated solution file to include GeneralMediaPOO project. Changed namespace in Program.cs and updated Main method to handle student data and calculate averages. Added GeneralMediaPOO.csproj with project properties and references. Created Student.cs defining Student class with properties and methods for handling grades.

--- GeneralMediumPOO/Program.cs
+++ GeneralMediumPOO/Program.cs
@@ -1,15 +1,55 @@
-﻿using System;
+﻿using GeneralMediumPOO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneralMediumPOO
+namespace GeneralMediaPOO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Title = "Student Media";
+
+            Console.WriteLine("How many students? ");
+            int nStudents = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Student[] students = new Student[nStudents];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.Clear();
+                Console.Write("Student #" + (i + 1) + " Name..: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Student #" + (i + 1) + " Exams: ");
+                int exams = int.Parse(Console.ReadLine());
+
+                students[i] = new Student(name, exams);
+
+                Console.WriteLine("Type the grades of student " + name);
+                students[i].SetGrades();
+            }
+
+            Console.Clear();
+
+            double generalMedia = 0;
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine("Student: " + student.Name);
+                Console.WriteLine("Media..: " + student.Media().ToString("F2"));
+                Console.WriteLine();
+                generalMedia += student.Media();
+            }
+
+            double finalResult = generalMedia / students.Length;
+
+            Console.WriteLine("General Media: " + finalResult.ToString("F2"));
         }
     }
 }
---
+++ GeneralMediumPOO/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralMediumPOO
+{
+    internal class Student
+    {
+        public string Name { get; private set; }
+        private double[] _grades;
+
+        public double Media()
+        { 
+            return CalcMedia();
+        }
+
+        public Student(string name, int exams)
+        {
+            Name = name;
+            _grades = new double[exams];
+        }
+
+        public void SetGrades()
+        {
+            for (int i = 0; i < _grades.Length; i++)
+            {
+                Console.Write("Grade #" + (i + 1) + ": ");
+                _grades[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcMedia()
+        {
+            double total = 0;
+            for (int i = 0; i < _grades.Length; i++)
+            {
+               total += _grades[i];
+            }
+
+            return total / _grades.Length;
+        }
+    }
+}
